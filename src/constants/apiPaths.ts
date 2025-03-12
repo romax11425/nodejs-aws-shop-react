45bdf2a8b8a33dd51aa9e37bf608a58516424935
romax11425 <romax114@gmail.com>
2025-03-12 05:05:55 +0300

    update apiPaths and CSV

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://p5f8iisg1i.execute-api.eu-west-1.amazonaws.com/prod",
+  product: "https://zs6ji7w1uh.execute-api.eu-west-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://mmfo9dqam9.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
