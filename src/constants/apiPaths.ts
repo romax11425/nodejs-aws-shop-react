ace1c9728a06fe99cbe5e3cc8dc6dcb88bbafae4
romax11425 <romax114@gmail.com>
2025-03-17 03:25:29 +0300

    update apiPaths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://24nktugwj1.execute-api.eu-west-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://pktvqbbb81.execute-api.eu-west-1.amazonaws.com/prod",
+  import: "https://c4gbxlo7nh.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
